036f52cebdb9cddafdbd9a44d97e3d5d458c7541
Marchiori <gustavomarchiori79@gmail.com>
2023-05-08 19:34:56 -0300

    Aprende mano
    pateta diabo

--- TDE.py
+++ TDE.py
@@ -15,34 +15,52 @@ bases_de_dados = {'terra': {}, 'mar': {}}
 
 for arquivo in lista_producao_terra:
     ano = os.path.splitext(os.path.basename(arquivo))[0].split('_')[0]
-    dataframe = pd.read_csv(arquivo, usecols=['Petróleo (bbl/dia)'], header=0, delimiter=';', decimal=',', low_memory=False)
+    dataframe = pd.read_csv(arquivo, usecols=['Estado', 'Período', 'Petróleo (bbl/dia)'], header=0, delimiter=';', decimal=',', low_memory=False)
     dataframe = dataframe.dropna()
     dataframe['Petróleo (bbl/dia)'] = dataframe['Petróleo (bbl/dia)'].astype(str).apply(locale.atof)
     if ano not in bases_de_dados['terra']: bases_de_dados['terra'][ano] = []
-    bases_de_dados['terra'][ano].append(dataframe['Petróleo (bbl/dia)'])
+    bases_de_dados['terra'][ano].append(dataframe)
 
 for arquivo in lista_producao_mar:
     ano = os.path.splitext(os.path.basename(arquivo))[0].split('_')[0]
-    dataframe = pd.read_csv(arquivo, usecols=['Petróleo (bbl/dia)'], header=0, delimiter=';', decimal=',', low_memory=False)
+    dataframe = pd.read_csv(arquivo, usecols=['Estado', 'Período', 'Petróleo (bbl/dia)'], header=0, delimiter=';', decimal=',', low_memory=False)
     dataframe = dataframe.dropna()
     dataframe['Petróleo (bbl/dia)'] = dataframe['Petróleo (bbl/dia)'].astype(str).apply(locale.atof)
     if ano not in bases_de_dados['mar']: bases_de_dados['mar'][ano] = []
-    bases_de_dados['mar'][ano].append(dataframe['Petróleo (bbl/dia)'])
+    bases_de_dados['mar'][ano].append(dataframe)
 
 producao_terra_anos = dict()
 producao_terra_geral = 0
 
 for ano, arquivos in bases_de_dados['terra'].items():
-    soma = round(sum(pd.concat(arquivos)), 2)
+    concat = pd.concat(arquivos)
+    soma = round(sum(concat['Petróleo (bbl/dia)']), 3)
     producao_terra_anos[ano] = soma
     producao_terra_geral+=soma
+producao_terra_geral = round(producao_terra_geral, 3)
 
 producao_mar_anos = dict()
 producao_mar_geral = 0
 
 for ano, arquivos in bases_de_dados['mar'].items():
-    soma = round(sum(pd.concat(arquivos)), 2)
+    concat = pd.concat(arquivos)
+    soma = round(sum(concat['Petróleo (bbl/dia)']), 3)
     producao_mar_anos[ano] = soma
     producao_mar_geral+=soma
+producao_mar_geral = round(producao_mar_geral, 3)
 
 producao_todos_os_anos = producao_terra_geral + producao_mar_geral
+
+print(producao_terra_geral, producao_mar_geral, producao_todos_os_anos)
+
+import matplotlib.pyplot as plt
+
+x = ['A', 'B', 'C']
+y = [10, 20, 30]
+plt.bar(x, y)
+
+plt.title('Pateta Diabo 2: O retorno')
+plt.xlabel('Pateta')
+plt.ylabel('Diabo')
+
+plt.show()
