ede0e5ae6ce83bb0164466a964510fc5c652cd4e
Marchiori <gustavomarchiori79@gmail.com>
2023-05-08 16:48:40 -0300

    Finalizadas todas as modificações necessárias

--- TDE.py
+++ TDE.py
@@ -1,35 +1,48 @@
 #Importando a biblioteca pandas para a manipulação e análise de dados.
 import pandas as pd
+import locale
 import glob
+import os
 
+locale.setlocale(locale.LC_NUMERIC, 'pt_BR.utf8')
 
-#Definindo o caminho para a pasta onde estão localizados seus arquivos CSV:
-caminho_pasta = "C:/Users/valte/OneDrive/Documentos/MeusProjetos/TDE/Produção_Combustivel/teste"
+caminho_pasta = "C:/Users/valte/OneDrive/Documentos/MeusProjetos/TDE/Produção_Combustivel"
 
-#Criando uma lista vazia para armazenar os DataFrames que serão criados a partir de cada arquivo CSV dentro da pasta:
-lista_producao = glob.glob(caminho_pasta + '/*.csv')
+lista_producao_mar = glob.glob(caminho_pasta + '/producao_mar/*.csv')
+lista_producao_terra = glob.glob(caminho_pasta + '/producao_terra/*.csv')
 
-#loop para ler cada arquivo CSV na pasta e adicionar seu DataFrame resultante à lista de DataFrames:
-petroleo = []
-# os.listdir() retorna uma lista com o nome de todos os arquivos e diretórios na pasta especificada.
-for nome_arquivo in lista_producao:
-  #if nome_arquivo.endswith('.csv') é para garantir que apenas arquivos CSV sejam lidos
-    
-      #os.path.join() é usada para combinar o caminho da pasta e o nome do arquivo em uma única string.
-        #pd.read_csv() é usada para ler cada arquivo CSV e criar um DataFrame. 
-        dataframe = pd.read_csv(nome_arquivo, usecols=['Petróleo (bbl/dia)'], header=0, delimiter=';', decimal=',')
-        print(dataframe)
-        '''lista_producao.append(dataframe)
+bases_de_dados = {'terra': {}, 'mar': {}}
 
+for arquivo in lista_producao_terra:
+    ano = os.path.splitext(os.path.basename(arquivo))[0].split('_')[0]
+    dataframe = pd.read_csv(arquivo, usecols=['Petróleo (bbl/dia)'], header=0, delimiter=';', decimal=',', low_memory=False)
+    dataframe = dataframe.dropna()
+    dataframe['Petróleo (bbl/dia)'] = dataframe['Petróleo (bbl/dia)'].astype(str).apply(locale.atof)
+    if ano not in bases_de_dados['terra']: bases_de_dados['terra'][ano] = []
+    bases_de_dados['terra'][ano].append(dataframe['Petróleo (bbl/dia)'])
 
-#juntando todos os DataFrames em um único DataFrame usando a função pd.concat():
-producao = pd.concat(lista_producao,axis=0, join='inner', ignore_index=True)
+for arquivo in lista_producao_mar:
+    ano = os.path.splitext(os.path.basename(arquivo))[0].split('_')[0]
+    dataframe = pd.read_csv(arquivo, usecols=['Petróleo (bbl/dia)'], header=0, delimiter=';', decimal=',', low_memory=False)
+    dataframe = dataframe.dropna()
+    dataframe['Petróleo (bbl/dia)'] = dataframe['Petróleo (bbl/dia)'].astype(str).apply(locale.atof)
+    if ano not in bases_de_dados['mar']: bases_de_dados['mar'][ano] = []
+    bases_de_dados['mar'][ano].append(dataframe['Petróleo (bbl/dia)'])
 
-#Formatando nosso objeto DataFrame para remover dos valores todos os pontos e logo após transformar todas as vírgulas em pontos.
-producao['Petróleo (bbl/dia)'] = producao['Petróleo (bbl/dia)'].apply(lambda x: x.replace(".", "").replace(",", "."))
+producao_terra_anos = dict()
+producao_terra_geral = 0
 
-#Transformando o tipo dos nossos valores dentro do objeto DataFrame (Series) para valores de ponto flutuante. 
-producao['Petróleo (bbl/dia)'] = producao['Petróleo (bbl/dia)'].astype(float)
+for ano, arquivos in bases_de_dados['terra'].items():
+    soma = round(sum(pd.concat(arquivos)), 2)
+    producao_terra_anos[ano] = soma
+    producao_terra_geral+=soma
 
-#Imprime a soma de toda a coluna presente no DataFrame com 2 valores de casa decimal.
-print(f'Soma dos valores presentes na coluna Petróleo (bbl/dia): {sum(producao["Petróleo (bbl/dia)"]):.2f}')'''
+producao_mar_anos = dict()
+producao_mar_geral = 0
+
+for ano, arquivos in bases_de_dados['mar'].items():
+    soma = round(sum(pd.concat(arquivos)), 2)
+    producao_mar_anos[ano] = soma
+    producao_mar_geral+=soma
+
+producao_todos_os_anos = producao_terra_geral + producao_mar_geral
